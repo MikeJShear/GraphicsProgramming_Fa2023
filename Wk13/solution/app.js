73d38fe08a9e13a92675eafce07dd0bb54178fb2
Michael Shear <mshear@csumb.edu>
2023-04-24 20:02:59 -0700

    WK 12/13

---
+++ Wk13/solution/app.js
@@ -0,0 +1,183 @@
+'use strict'
+
+var gl;
+
+var appInput = new Input();
+var time = new Time();
+var camera = new OrbitCamera(appInput);
+
+var sphereGeometry = null; // this will be created after loading from a file
+var pointLightGeometry = null;
+var groundGeometry = null;
+var barrelGeometry = null;
+
+var projectionMatrix = new Matrix4();
+var lightPosition = new Vector4(4, 1.5, 0, 0);
+
+// the shader that will be used by each piece of geometry (they could each use their own shader but in this case it will be the same)
+var phongShaderProgram;
+
+// auto start the app when the html page is ready
+window.onload = window['initializeAndStartRendering'];
+
+// we need to asynchronously fetch files from the "server" (your local hard drive)
+var loadedAssets = {
+    phongTextVS: null, phongTextFS: null,
+    sphereJSON: null,
+    barrelJSON: null,
+    marbleImage: null,
+    crackedMudImage: null,
+    barrelImage: null
+};
+
+// -------------------------------------------------------------------------
+function initializeAndStartRendering() {
+    initGL();
+    loadAssets(function() {
+        createShaders(loadedAssets);
+        createScene();
+
+        updateAndRender();
+    });
+}
+
+// -------------------------------------------------------------------------
+function initGL(canvas) {
+    var canvas = document.getElementById("webgl-canvas");
+
+    try {
+        gl = canvas.getContext("webgl");
+        gl.canvasWidth = canvas.width;
+        gl.canvasHeight = canvas.height;
+
+        gl.enable(gl.DEPTH_TEST);
+    } catch (e) {}
+
+    if (!gl) {
+        alert("Could not initialise WebGL, sorry :-(");
+    }
+}
+
+// -------------------------------------------------------------------------
+function loadAssets(onLoadedCB) {
+    var filePromises = [
+        fetch('./shaders/phong.vs.glsl').then((response) => { return response.text(); }),
+        fetch('./shaders/phong.pointlit.fs.glsl').then((response) => { return response.text(); }),
+        fetch('./data/sphere.json').then((response) => { return response.json(); }),
+        fetch('./data/barrel.json').then((response) => { return response.json(); }),
+        loadImage('./data/marble.jpg'),
+        loadImage('./data/crackedMud.png'),
+        loadImage('./data/barrel.png')
+    ];
+
+    Promise.all(filePromises).then(function(values) {
+        // Assign loaded data to our named variables
+        loadedAssets.phongTextVS = values[0];
+        loadedAssets.phongTextFS = values[1];
+        loadedAssets.sphereJSON = values[2];
+        loadedAssets.barrelJSON = values[3];
+        loadedAssets.marbleImage = values[4];
+        loadedAssets.crackedMudImage = values[5];
+        loadedAssets.barrelImage = values[6];
+    }).catch(function(error) {
+        console.error(error.message);
+    }).finally(function() {
+        onLoadedCB();
+    });
+}
+
+// -------------------------------------------------------------------------
+function createShaders(loadedAssets) {
+    phongShaderProgram = createCompiledAndLinkedShaderProgram(loadedAssets.phongTextVS, loadedAssets.phongTextFS);
+
+    phongShaderProgram.attributes = {
+        vertexPositionAttribute: gl.getAttribLocation(phongShaderProgram, "aVertexPosition"),
+        vertexNormalsAttribute: gl.getAttribLocation(phongShaderProgram, "aNormal"),
+        vertexTexcoordsAttribute: gl.getAttribLocation(phongShaderProgram, "aTexcoords")
+    };
+
+    phongShaderProgram.uniforms = {
+        worldMatrixUniform: gl.getUniformLocation(phongShaderProgram, "uWorldMatrix"),
+        viewMatrixUniform: gl.getUniformLocation(phongShaderProgram, "uViewMatrix"),
+        projectionMatrixUniform: gl.getUniformLocation(phongShaderProgram, "uProjectionMatrix"),
+        lightPositionUniform: gl.getUniformLocation(phongShaderProgram, "uLightPosition"),
+        cameraPositionUniform: gl.getUniformLocation(phongShaderProgram, "uCameraPosition"),
+        textureUniform: gl.getUniformLocation(phongShaderProgram, "uTexture"),
+    };
+}
+
+// -------------------------------------------------------------------------
+function createScene() {
+    groundGeometry = new WebGLGeometryQuad(gl, phongShaderProgram);
+    groundGeometry.create(loadedAssets.crackedMudImage);
+
+    var scale = new Matrix4().makeScale(10.0, 10.0, 10.0);
+
+    // compensate for the model being flipped on its side
+    var rotation = new Matrix4().makeRotationX(-90);
+
+    groundGeometry.worldMatrix.makeIdentity();
+    groundGeometry.worldMatrix.multiply(rotation).multiply(scale);
+
+    sphereGeometry = new WebGLGeometryJSON(gl, phongShaderProgram);
+    sphereGeometry.create(loadedAssets.sphereJSON, loadedAssets.marbleImage);
+
+    barrelGeometry = new WebGLGeometryJSON(gl, phongShaderProgram);
+    barrelGeometry.create(loadedAssets.barrelJSON, loadedAssets.barrelImage);
+
+    // Scaled it down so that the diameter is 3
+    var scale = new Matrix4().makeScale(0.03, 0.03, 0.03);
+    var Barrelscale = new Matrix4().makeScale(0.3, 0.3, 0.3);
+
+    // raise it by the radius to make it sit on the ground
+    var translation = new Matrix4().makeTranslation(0, 1.5, 0);
+    var barrelTranslation = new Matrix4().makeTranslation(-5,2,-5);
+
+    sphereGeometry.worldMatrix.makeIdentity();
+    sphereGeometry.worldMatrix.multiply(translation).multiply(scale);
+    barrelGeometry.worldMatrix.makeIdentity();
+    barrelGeometry.worldMatrix.multiply(barrelTranslation).multiply(Barrelscale);
+
+}
+
+// -------------------------------------------------------------------------
+function updateAndRender() {
+    requestAnimationFrame(updateAndRender);
+
+    var aspectRatio = gl.canvasWidth / gl.canvasHeight;
+
+    var yaw = 0, pitch = 0;
+    yaw -= 1;
+    // if (appInput.d) yaw += 1;
+    // if (appInput.w) pitch -= 1;
+    // if (appInput.s) pitch += 1;
+
+
+
+    var yawMatrix = new Matrix4().makeRotationY(45.0 * time.deltaTime * yaw);
+    var pitchMatrix = new Matrix4().makeRotationX(45.0 * time.deltaTime * pitch);
+    var rotationMatrix = pitchMatrix.clone().multiply(yawMatrix);
+
+    lightPosition = rotationMatrix.multiplyVector(lightPosition);
+
+    time.update();
+    camera.update(time.deltaTime);
+
+    // specify what portion of the canvas we want to draw to (all of it, full width and height)
+    gl.viewport(0, 0, gl.canvasWidth, gl.canvasHeight);
+
+    // this is a new frame so let's clear out whatever happened last frame
+    gl.clearColor(0.707, 0.707, 1, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.useProgram(phongShaderProgram);
+    var uniforms = phongShaderProgram.uniforms;
+    var cameraPosition = camera.getPosition();
+    gl.uniform3f(uniforms.lightPositionUniform, lightPosition.x, lightPosition.y, lightPosition.z);
+    gl.uniform3f(uniforms.cameraPositionUniform, cameraPosition.x, cameraPosition.y, cameraPosition.z);
+
+    projectionMatrix.makePerspective(45, aspectRatio, 0.1, 1000);
+    groundGeometry.render(camera, projectionMatrix, phongShaderProgram);
+    sphereGeometry.render(camera, projectionMatrix, phongShaderProgram);
+    barrelGeometry.render(camera, projectionMatrix, phongShaderProgram);
+}
